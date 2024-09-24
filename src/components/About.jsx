74a62e1d919e2de18fd3fc652465319b22b12ca2
maimohammed928 <maimohammed928@gmail.com>
2024-09-24 09:09:08 +0300

    mm

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,21 @@ import Link from 'next/link'
 import React from 'react'
 import { fetchData } from '../../utils/api';
 import DOMPurify from 'isomorphic-dompurify';
-   
-const i18nNamespaces = ["home"];
 
-const About = async ({params}) => {
-    const { locale } = params
+
+const About = async ({ params }) => {
 
     const truncateText = (text, wordCount) => {
         return text?.split(' ').slice(0, wordCount).join(' ') + '...';
     };
+    const i18nNamespaces = ["home"];
+
+    const { locale } = params
+
 
     const { t } = await initTranslations(locale, i18nNamespaces)
-    const Data =await fetchData(`api/about-us`,locale)
-      const      aboutData =Data?.data
+    const Data = await fetchData(`api/about-us`, locale)
+    const aboutData = Data?.data
 
     return (
         <section className='bg-light_gray'>
@@ -23,7 +25,7 @@ const About = async ({params}) => {
                 <div className='block lg:flex justify-between '>
                     <div className=' w-full lg:w-[45%]  text-center lg:text-start'>
                         <h3 className='text-xl lg:text-3xl'>{t(aboutData?.title)}</h3>
-                        <div className='pt-5 pb-10 text-meduim_gray  text-[15px] leading-8 ' dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(t(truncateText(aboutData?.details || '' ,50)))}}   />
+                        <div className='pt-5 pb-10 text-meduim_gray  text-[15px] leading-8 ' dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(t(truncateText(aboutData?.details || '', 50))) }} />
                         <Link href={'/about'} className='bg-primary_Color_Light hover:bg-primary_Color_dark px-5 py-3 text-white'>Read More</Link>
                     </div>
                     <div className='w-full lg:w-[45%] mt-10 lg:mt-0'>
